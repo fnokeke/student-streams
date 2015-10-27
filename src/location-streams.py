f05684ab6ee803bc47ee4135003e0b3dfd8a121a
Fabian Okeke <fnokeke@gmail.com>
2015-10-27 19:31:24 -0400

    seaborn plot used first time; now plotting time of day vs location label

--- src/location-streams.py
+++ src/location-streams.py
@@ -16,16 +16,12 @@ import fiona
 import sklearn
 import time
 import datetime
-import matplotlib.dates as mdates
+import calendar
+import seaborn as sns
 
-from shapely.geometry import Point, Polygon, MultiPoint, MultiPolygon
-from shapely.prepared import prep
-from mpl_toolkits.basemap import Basemap
-from matplotlib.collections import PatchCollection
-from descartes import PolygonPatch
+import matplotlib.dates as mdates
 from matplotlib import pyplot as plt
-from sklearn.metrics.pairwise import pairwise_distances_argmin
-from ggplot import *
+
 
 %matplotlib inline
 
@@ -102,7 +98,7 @@ ld.head()
 
 # <codecell>
 
-no_of_weeks = 1
+no_of_weeks = 4
 no_of_days = no_of_weeks * 7
 
 start_date = ld[ld.date=='2014-11-09'].date.head(1)
@@ -118,7 +114,7 @@ ld_wk = ld[ld.date.isin(dates)]
 print "Week length:", len(ld_wk)
 print dates[:5]
 
-#type(pd.to_datetime("2014-11-09") + datetime.timedelta(days=-2))
+# type(pd.to_datetime("2014-11-09") + datetime.timedelta(days=-2))
 
 # <markdowncell>
 
@@ -197,8 +193,8 @@ ld_wk[ld_wk['date'] == ld_wk.date[0]].iloc[rows]
 # <codecell>
 
 loc_patterns = ld_wk[['datetime', 'loc_label']]
-loc_patterns['freq'] = ld_wk['loc_label']
 loc_patterns = loc_patterns.set_index('datetime')
+loc_patterns['freq'] = 0
 loc_group = loc_patterns.groupby(['loc_label']).resample('6H',how=len)
 loc_group = loc_group.reset_index()
 loc_group.head()
@@ -214,10 +210,90 @@ loc_group.unstack().head()
 
 # <codecell>
 
-loc_plot = loc_group.unstack().plot(kind='barh', stacked=True, title="Location Freq by Date", figsize=(15, 10))
+loc_plot = loc_group.unstack().plot(kind='barh', stacked=True, title="Location Freq by Date", figsize=(20, 20))
 loc_plot.set_xlabel("Frequency")
 loc_plot.set_ylabel("Time")
-loc_plot.legend(["Home","Other","Work"], loc=1,ncol=1)
+loc_plot.legend(["Home","Other","Work"],loc='center left', bbox_to_anchor=(1, 0.5), fancybox=True, shadow=True)
+
+# <markdowncell>
+
+# #Mark every day by the day name
+
+# <codecell>
+
+weekday_patterns = ld_wk[['datetime', 'loc_label']]
+weekday_patterns['weekday'] = weekday_patterns.datetime.map(lambda x: calendar.day_name[x.weekday()])
+weekday_patterns.head()
+
+# <codecell>
+
+weekday_patterns['freq'] = 0
+weekday_patterns = weekday_patterns.set_index('datetime')
+weekday_patterns = weekday_patterns.groupby(['weekday', 'loc_label']).resample('D', how=len)
+weekday_patterns.head()
+
+# <codecell>
+
+shorter_df = weekday_patterns.reset_index()[['weekday', 'loc_label', 'freq']]
+shorter_df.head()
+
+# <codecell>
+
+shorter_df = shorter_df.groupby(['weekday', 'loc_label']).sum().unstack()
+shorter_df.head(20)
+
+# <codecell>
+
+order_of_weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+shorter_df.index = pd.Categorical(shorter_df.index, order_of_weekdays)
+shorter_df = shorter_df.sort_index()
+sh_plot = shorter_df.plot()
+sh_plot.legend(["Home","Other","Work"],loc='center left', bbox_to_anchor=(1, 0.5), fancybox=True, shadow=True)
+
+# <codecell>
+
+sh_plot = shorter_df.plot(kind='barh', stacked=True)
+sh_plot.legend(["Home","Other","Work"],loc='center left', bbox_to_anchor=(1, 0.5), fancybox=True, shadow=True)
+
+# <markdowncell>
+
+# #Where you are by dates
+
+# <codecell>
+
+day_time_patterns = ld_wk[['datetime', 'loc_label']]
+
+day_time_patterns.loc[:,'loc_label'] = pd.Categorical(day_time_patterns['loc_label'])
+
+day_time_patterns.loc[:,'hr'] = day_time_patterns.datetime.map(lambda x: int(x.strftime("%H")) + float(x.strftime("%M"))/60)
+day_time_patterns.loc[:,'hr'] = pd.Categorical(day_time_patterns['hr'])
+
+day_time_patterns.loc[:,'weekday'] = day_time_patterns.datetime.map(lambda x: calendar.day_name[x.weekday()])
+day_time_patterns.loc[:,'weekday'] = pd.Categorical(day_time_patterns['weekday'], order_of_weekdays)
+
+day_time_patterns.head()
+
+# <codecell>
+
+day_time_patterns.hr = day_time_patterns.hr.astype(float)
+day_time_patterns.dtypes
+
+# <markdowncell>
+
+# #Show location by time of day (seaborn plot)
+
+# <codecell>
+
+sns.set(style="whitegrid", color_codes=True)
+sns.set_context({"figure.figsize": (10, 7)})
+
+axes = sns.stripplot(x="date", y="hr", hue="loc_label", data=day_time_patterns, jitter=True);
+axes.set_ylim(0,25)
+
+labels = ["Midnight", "5am", "10am", "3pm", "8pm", "Midnight"]
+axes.set_yticklabels(labels)
+
+axes.legend(["Home","Other","Work"],loc='center left', bbox_to_anchor=(1, 0.5), fancybox=True, shadow=True)
 
 # <codecell>
 
