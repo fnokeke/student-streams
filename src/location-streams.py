7867c29aeaa868ada0e9d84eccc0d4482cf6c914
Fabian Okeke <fnokeke@gmail.com>
2015-10-31 00:14:04 -0400

    removed bokeh. it's not that beneficial and the unplanned zooming during scrolling is annoying

--- src/location-streams.py
+++ src/location-streams.py
@@ -1,11 +1,9 @@
-# -*- coding: utf-8 -*-
-# <nbformat>3.0</nbformat>
 
-# <markdowncell>
+# coding: utf-8
 
 # #Get relevant libraries
 
-# <codecell>
+# In[237]:
 
 import ujson as json
 import pprint
@@ -23,14 +21,13 @@ import matplotlib.dates as mdates
 from matplotlib import pyplot as plt
 from ggplot import *
 
-%matplotlib inline
+get_ipython().magic(u'matplotlib inline')
 pd.options.mode.chained_assignment = None  # remove default='warn'
 
-# <markdowncell>
 
 # #Import data
 
-# <codecell>
+# In[238]:
 
 filename = "/Users/fnokeke/dev/student-streams/dataset/LocationHistory.json"
 
@@ -41,19 +38,17 @@ ld = pd.DataFrame(raw['locations'])
 
 print("Number of items in data: %d") % len(ld)
 
-# <markdowncell>
 
 # #Free up memory
 
-# <codecell>
+# In[239]:
 
 del raw
 
-# <markdowncell>
 
 # #Convert to typical units
 
-# <codecell>
+# In[240]:
 
 ld['latitudeE7'] = ld['latitudeE7']/float(1e7)
 ld['longitudeE7'] = ld['longitudeE7']/float(1e7)
@@ -65,11 +60,10 @@ ld['time'] = ld.datetime.map(lambda x: x.strftime("%H:%M:%S"))
 ld['hr'] = ld.datetime.map(lambda x: x.strftime("%H"))
 ld['min'] = ld.datetime.map(lambda x: x.strftime("%M"))
 
-# <markdowncell>
 
 # #Rename fields
 
-# <codecell>
+# In[241]:
 
 ld.rename(columns={
 'latitudeE7': 'latitude',
@@ -77,27 +71,24 @@ ld.rename(columns={
 'timestampMs': 'timestamp'
 }, inplace=True)
 
-# <markdowncell>
 
 # #Understand the data types of each column
 
-# <codecell>
+# In[242]:
 
 ld.dtypes
 
-# <markdowncell>
 
 # #Get a glimpse of raw data
 
-# <codecell>
+# In[243]:
 
 ld.head()
 
-# <markdowncell>
 
 # #Get only a specific number of weeks of data from specific start date
 
-# <codecell>
+# In[244]:
 
 no_of_weeks = 2
 no_of_days = no_of_weeks * 7
@@ -117,29 +108,26 @@ print dates[:5]
 
 # type(pd.to_datetime("2014-11-09") + datetime.timedelta(days=-2))
 
-# <markdowncell>
 
 # #Ignore locations with accuracy estimates over 1000m
 
-# <codecell>
+# In[245]:
 
 ld_wk = ld_wk[ld_wk.accuracy < 1000]
 ld_wk.reset_index(drop=True, inplace=True)
 print("Number of items in data: %d") % len(ld_wk)
 
-# <markdowncell>
 
 # #Select only columns of interest
 
-# <codecell>
+# In[246]:
 
 ld_wk = ld_wk[['latitude', 'longitude', 'datetime', 'date']]
 
-# <markdowncell>
 
 # #Specify places of interest in Ithaca and ignore locations outside Ithaca
 
-# <codecell>
+# In[247]:
 
 home = (42.446594, -76.493736)
 work = (42.444877, -76.480814)
@@ -153,11 +141,10 @@ print("Number of items in data: %d") % len(ld_wk)
 ld_wk.head()
 print "No of unique dates:", len(set(ld_wk.date))
 
-# <markdowncell>
 
 # #Label every location as either home, work or other
 
-# <codecell>
+# In[248]:
 
 lat_error_margin = 0.0005
 lon_error_margin = 0.005
@@ -178,20 +165,18 @@ def get_loc_label(df, points):
 
 ld_wk['loc_label'] = ld_wk.apply(lambda x: get_loc_label(x, POINTS), axis='columns')
 
-# <markdowncell>
 
 # #Show sample locations of home, work, other
 
-# <codecell>
+# In[249]:
 
 rows = [0,1,59,60,61,62]
 ld_wk[ld_wk['date'] == ld_wk.date[0]].iloc[rows]
 
-# <markdowncell>
 
 # #Resample location patterns data by different time period
 
-# <codecell>
+# In[250]:
 
 loc_patterns = ld_wk[['datetime', 'loc_label']]
 loc_patterns = loc_patterns.set_index('datetime')
@@ -200,50 +185,56 @@ loc_group = loc_patterns.groupby(['loc_label']).resample('6H',how=len)
 loc_group = loc_group.reset_index()
 loc_group.head()
 
-# <codecell>
+
+# In[251]:
 
 loc_group = loc_group.groupby(['datetime','loc_label']).sum()
 loc_group.head()
 
-# <codecell>
+
+# In[252]:
 
 loc_group.unstack().head()
 
-# <codecell>
+
+# In[253]:
 
 loc_plot = loc_group.unstack().plot(kind='barh', stacked=True, title="Location Freq by Date", figsize=(20, 20))
 loc_plot.set_xlabel("Frequency")
 loc_plot.set_ylabel("Time")
 loc_plot.legend(["Home","Other","Work"],loc='center left', bbox_to_anchor=(1, 0.5), fancybox=True, shadow=True)
 
-# <markdowncell>
 
 # #Mark every day by the day name
 
-# <codecell>
+# In[254]:
 
 weekday_patterns = ld_wk[['datetime', 'loc_label']]
 weekday_patterns['weekday'] = weekday_patterns.datetime.map(lambda x: calendar.day_name[x.weekday()])
 weekday_patterns.head()
 
-# <codecell>
+
+# In[255]:
 
 weekday_patterns['freq'] = 0
 weekday_patterns = weekday_patterns.set_index('datetime')
 weekday_patterns = weekday_patterns.groupby(['weekday', 'loc_label']).resample('D', how=len)
 weekday_patterns.head()
 
-# <codecell>
+
+# In[256]:
 
 shorter_df = weekday_patterns.reset_index()[['weekday', 'loc_label', 'freq']]
 shorter_df.head()
 
-# <codecell>
+
+# In[257]:
 
 shorter_df = shorter_df.groupby(['weekday', 'loc_label']).sum().unstack()
 shorter_df.head(20)
 
-# <codecell>
+
+# In[258]:
 
 order_of_weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 shorter_df.index = pd.Categorical(shorter_df.index, order_of_weekdays)
@@ -251,16 +242,16 @@ shorter_df = shorter_df.sort_index()
 sh_plot = shorter_df.plot()
 sh_plot.legend(["Home","Other","Work"],loc='center left', bbox_to_anchor=(1, 0.5), fancybox=True, shadow=True)
 
-# <codecell>
+
+# In[259]:
 
 sh_plot = shorter_df.plot(kind='barh', stacked=True)
 sh_plot.legend(["Home","Other","Work"],loc='center left', bbox_to_anchor=(1, 0.5), fancybox=True, shadow=True)
 
-# <markdowncell>
 
 # #Where you are by dates
 
-# <codecell>
+# In[260]:
 
 day_time_patterns = ld_wk[['datetime', 'loc_label']]
 
@@ -274,16 +265,16 @@ day_time_patterns.loc[:,'weekday'] = pd.Categorical(day_time_patterns['weekday']
 
 day_time_patterns.head()
 
-# <codecell>
+
+# In[261]:
 
 day_time_patterns.hr = day_time_patterns.hr.astype(float)
 day_time_patterns.dtypes
 
-# <markdowncell>
 
 # #Show location by time of day (seaborn plot)
 
-# <codecell>
+# In[262]:
 
 sns.set(style="whitegrid", color_codes=True)
 sns.set_context({"figure.figsize": (10, 7)})
@@ -296,7 +287,8 @@ axes.set_yticklabels(labels)
 
 axes.legend(["Home","Other","Work"],loc='center left', bbox_to_anchor=(1, 0.5), fancybox=True, shadow=True)
 
-# <codecell>
+
+# In[263]:
 
 def get_day_num(day): 
     if day == 'Monday':
@@ -316,21 +308,17 @@ def get_day_num(day):
 
 day_time_patterns['day_value'] = day_time_patterns.weekday.astype(str).map(lambda x: get_day_num(x))
 
-# <codecell>
+
+# In[264]:
 
 day_time_patterns.rename(columns={'loc_label': 'Location'}, inplace=True)
 day_time_patterns.head(10)
 
-# <codecell>
+
+# In[265]:
 
 weekday_label = ("Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun")
 time_label = ("Midnight", "5am", "10am", "3pm", "8pm", "Midnight")
 
-ggplot(day_time_patterns, aes('day_value', 'hr', color='Location')) + \
-    geom_point() + \
-    scale_x_continuous(name = "Day of Week", breaks=range(1,8), labels=weekday_label) + \
-    scale_y_continuous(name="Time of Day", limits=(0,25), labels=time_label)
-
-# <codecell>
-
+ggplot(day_time_patterns, aes('day_value', 'hr', color='Location')) +     geom_point() +     scale_x_continuous(name = "Day of Week", breaks=range(1,8), labels=weekday_label) +     scale_y_continuous(name="Time of Day", limits=(0,25), labels=time_label)
 
