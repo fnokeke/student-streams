66d15e19d3d1d53c21b769f522c1e044128ddb02
Fabian Okeke <fnokeke@gmail.com>
2015-10-31 17:15:38 -0400

    removed seaborn plots and now using ggplot or matplotlib

--- src/location-streams.py
+++ src/location-streams.py
@@ -1,17 +1,15 @@
 
 # coding: utf-8
 
-# #Get relevant libraries
+# # Get relevant libraries
 
-# In[237]:
+# In[299]:
 
 import ujson as json
 import pprint
 import pandas as pd
 import numpy as np
 import datetime
-import fiona
-import sklearn
 import time
 import datetime
 import calendar
@@ -21,49 +19,39 @@ import matplotlib.dates as mdates
 from matplotlib import pyplot as plt
 from ggplot import *
 
+
+# # All plots should be inside notebook
+
+# In[300]:
+
 get_ipython().magic(u'matplotlib inline')
 pd.options.mode.chained_assignment = None  # remove default='warn'
 
 
-# #Import data
+# # Import data
 
-# In[238]:
+# In[301]:
 
 filename = "/Users/fnokeke/dev/student-streams/dataset/LocationHistory.json"
-
 with open(filename) as json_file:
     raw = json.load(json_file)
 
 ld = pd.DataFrame(raw['locations'])
-
 print("Number of items in data: %d") % len(ld)
 
-
-# #Free up memory
-
-# In[239]:
-
+# free up memory
 del raw
 
 
-# #Convert to typical units
+# # Convert to typical units and rename columns
 
-# In[240]:
+# In[303]:
 
 ld['latitudeE7'] = ld['latitudeE7']/float(1e7)
 ld['longitudeE7'] = ld['longitudeE7']/float(1e7)
 ld['timestampMs'] = ld['timestampMs'].map(lambda x: float(x)/1000)
 ld['datetime'] = ld.timestampMs.map(datetime.datetime.fromtimestamp)
-ld['dt2'] = ld.datetime.map(lambda x: x.strftime("%Y-%m-%d %H:%M:%S"))
-ld['date'] = ld.datetime.map(lambda x: x.strftime("%Y-%m-%d"))
-ld['time'] = ld.datetime.map(lambda x: x.strftime("%H:%M:%S"))
-ld['hr'] = ld.datetime.map(lambda x: x.strftime("%H"))
-ld['min'] = ld.datetime.map(lambda x: x.strftime("%M"))
-
-
-# #Rename fields
-
-# In[241]:
+ld['date'] = ld.datetime.map(lambda x: x.strftime("%Y-%m-%d")) # TODO: remove
 
 ld.rename(columns={
 'latitudeE7': 'latitude',
@@ -72,31 +60,29 @@ ld.rename(columns={
 }, inplace=True)
 
 
-# #Understand the data types of each column
+# # Glimpse of raw data
 
-# In[242]:
+# In[336]:
 
-ld.dtypes
-
-
-# #Get a glimpse of raw data
-
-# In[243]:
-
+print ld.dtypes
 ld.head()
 
 
-# #Get only a specific number of weeks of data from specific start date
+# # Get only a specific number of weeks of data from specific start date
 
-# In[244]:
+# In[337]:
 
-no_of_weeks = 2
+no_of_weeks = 1
 no_of_days = no_of_weeks * 7
+
+
+# # Get date in string format
+
+# In[338]:
 
 start_date = ld[ld.date=='2014-11-09'].date.head(1)
 start_date = pd.to_datetime(start_date)
 
-print type(start_date)
 dates = []
 for i in range(no_of_days):
     x = (start_date + datetime.timedelta(days=-i)).to_string()[9:] # horrible tweak to get date in string format
@@ -106,28 +92,26 @@ ld_wk = ld[ld.date.isin(dates)]
 print "Week length:", len(ld_wk)
 print dates[:5]
 
-# type(pd.to_datetime("2014-11-09") + datetime.timedelta(days=-2))
 
+# # Ignore locations with accuracy estimates over 1000m
 
-# #Ignore locations with accuracy estimates over 1000m
-
-# In[245]:
+# In[339]:
 
 ld_wk = ld_wk[ld_wk.accuracy < 1000]
 ld_wk.reset_index(drop=True, inplace=True)
 print("Number of items in data: %d") % len(ld_wk)
 
 
-# #Select only columns of interest
+# # Select only columns of interest
 
-# In[246]:
+# In[340]:
 
 ld_wk = ld_wk[['latitude', 'longitude', 'datetime', 'date']]
 
 
-# #Specify places of interest in Ithaca and ignore locations outside Ithaca
+# # Specify places of interest in Ithaca and ignore locations outside Ithaca
 
-# In[247]:
+# In[341]:
 
 home = (42.446594, -76.493736)
 work = (42.444877, -76.480814)
@@ -142,9 +126,9 @@ ld_wk.head()
 print "No of unique dates:", len(set(ld_wk.date))
 
 
-# #Label every location as either home, work or other
+# # Label every location as either home, work or other
 
-# In[248]:
+# In[342]:
 
 lat_error_margin = 0.0005
 lon_error_margin = 0.005
@@ -166,17 +150,17 @@ def get_loc_label(df, points):
 ld_wk['loc_label'] = ld_wk.apply(lambda x: get_loc_label(x, POINTS), axis='columns')
 
 
-# #Show sample locations of home, work, other
+# # Show sample locations of home, work, other
 
-# In[249]:
+# In[343]:
 
 rows = [0,1,59,60,61,62]
 ld_wk[ld_wk['date'] == ld_wk.date[0]].iloc[rows]
 
 
-# #Resample location patterns data by different time period
+# # Resample location patterns data by different time period
 
-# In[250]:
+# In[344]:
 
 loc_patterns = ld_wk[['datetime', 'loc_label']]
 loc_patterns = loc_patterns.set_index('datetime')
@@ -186,35 +170,94 @@ loc_group = loc_group.reset_index()
 loc_group.head()
 
 
-# In[251]:
+# In[345]:
 
 loc_group = loc_group.groupby(['datetime','loc_label']).sum()
 loc_group.head()
 
 
-# In[252]:
+# In[346]:
 
 loc_group.unstack().head()
 
 
-# In[253]:
+# In[754]:
 
-loc_plot = loc_group.unstack().plot(kind='barh', stacked=True, title="Location Freq by Date", figsize=(20, 20))
+loc_plot = loc_group.unstack().plot(kind='barh', stacked=True, title="Location Freq by Date", figsize=(10,5))
 loc_plot.set_xlabel("Frequency")
 loc_plot.set_ylabel("Time")
-loc_plot.legend(["Home","Other","Work"],loc='center left', bbox_to_anchor=(1, 0.5), fancybox=True, shadow=True)
+r = loc_plot.legend(["Home","Other","Work"],loc='center left', bbox_to_anchor=(1, 0.5), fancybox=True, shadow=True)
 
 
-# #Mark every day by the day name
+# # Mark every day by the day name
 
-# In[254]:
+# In[674]:
 
 weekday_patterns = ld_wk[['datetime', 'loc_label']]
 weekday_patterns['weekday'] = weekday_patterns.datetime.map(lambda x: calendar.day_name[x.weekday()])
+weekday_patterns['day'] = weekday_patterns.datetime.map(lambda x: x.weekday())
 weekday_patterns.head()
 
 
-# In[255]:
+# # ggplot of day_of_week vs location frequency (stacked bar)
+
+# In[556]:
+
+weekday_label = ("Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun")
+
+ggplot(weekday_patterns, aes("day", fill='loc_label')) + geom_bar() +     scale_x_continuous(name="Day of Week", labels=weekday_label) +     scale_y_continuous(name="Frequency", labels="comma") +     ggtitle("Location Frequency by Day of Week") +     theme_seaborn()
+
+
+# # ggplot of day_of_week vs location frequency (multiple lines)
+
+# In[612]:
+
+wkdf = weekday_patterns[['day', 'loc_label']]
+wkdf['freq'] = 0
+wkdf = wkdf.groupby(['day', 'loc_label']).count().reset_index()
+
+ggplot(wkdf, aes('day', 'freq', color='loc_label')) + geom_line() +     scale_x_continuous(name="Day of Week", labels=weekday_label) +     scale_y_continuous(name="Frequency", labels="comma") +     ggtitle("Location Frequency by Day of Week") +     theme_seaborn()
+
+
+# # Show location frequency by date (ggplot)
+
+# In[753]:
+
+date_df = weekday_patterns[['datetime', 'loc_label']]
+date_df['freq'] = 0
+
+date_df = date_df.set_index('datetime')
+date_df = date_df.groupby('loc_label').resample('D', how='count').reset_index()
+
+ggplot(date_df, aes('datetime', 'freq', color='loc_label')) +     geom_line() +     scale_x_date(name="Date", labels="%b %d %-I %p", breaks=date_breaks("8 hours")) +     ggtitle("Frequency of Location by Time") +     theme_bw()
+
+
+# # Datetime by barchart (ggplot)
+
+# In[728]:
+
+date_df = weekday_patterns[['datetime', 'loc_label']]
+date_df['freq'] = 0
+
+pp = date_df.set_index('datetime').groupby('loc_label').resample('D', how='count').reset_index()
+pp
+
+
+# In[739]:
+
+qq=pd.melt(pp, id_vars=['datetime','loc_label'])
+qq['x'] = qq.index
+qq.head(10)
+
+
+# In[744]:
+
+ggplot(qq, aes('x', weight='value', fill='loc_label')) +     geom_bar() +     ggtitle("Frequency of Location by Time")
+
+
+# # Resample dates by specific period (hours, days, weeks)
+
+# In[349]:
 
 weekday_patterns['freq'] = 0
 weekday_patterns = weekday_patterns.set_index('datetime')
@@ -222,73 +265,27 @@ weekday_patterns = weekday_patterns.groupby(['weekday', 'loc_label']).resample('
 weekday_patterns.head()
 
 
-# In[256]:
+# # Where you are by dates
 
-shorter_df = weekday_patterns.reset_index()[['weekday', 'loc_label', 'freq']]
-shorter_df.head()
-
-
-# In[257]:
-
-shorter_df = shorter_df.groupby(['weekday', 'loc_label']).sum().unstack()
-shorter_df.head(20)
-
-
-# In[258]:
-
-order_of_weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-shorter_df.index = pd.Categorical(shorter_df.index, order_of_weekdays)
-shorter_df = shorter_df.sort_index()
-sh_plot = shorter_df.plot()
-sh_plot.legend(["Home","Other","Work"],loc='center left', bbox_to_anchor=(1, 0.5), fancybox=True, shadow=True)
-
-
-# In[259]:
-
-sh_plot = shorter_df.plot(kind='barh', stacked=True)
-sh_plot.legend(["Home","Other","Work"],loc='center left', bbox_to_anchor=(1, 0.5), fancybox=True, shadow=True)
-
-
-# #Where you are by dates
-
-# In[260]:
+# In[393]:
 
 day_time_patterns = ld_wk[['datetime', 'loc_label']]
 
-day_time_patterns.loc[:,'loc_label'] = pd.Categorical(day_time_patterns['loc_label'])
+day_time_patterns['hr'] = day_time_patterns.datetime.map(lambda x: int(x.strftime("%H")) + float(x.strftime("%M"))/60)
+day_time_patterns.hr = pd.Categorical(day_time_patterns.hr)
+day_time_patterns.hr = day_time_patterns.hr.astype(float)
 
-day_time_patterns.loc[:,'hr'] = day_time_patterns.datetime.map(lambda x: int(x.strftime("%H")) + float(x.strftime("%M"))/60)
-day_time_patterns.loc[:,'hr'] = pd.Categorical(day_time_patterns['hr'])
+# TODO: remove this weekday code if already using day_value
+day_time_patterns['weekday'] = day_time_patterns.datetime.map(lambda x: calendar.day_name[x.weekday()])
+day_time_patterns.weekday = pd.Categorical(day_time_patterns.weekday, order_of_weekdays)
 
-day_time_patterns.loc[:,'weekday'] = day_time_patterns.datetime.map(lambda x: calendar.day_name[x.weekday()])
-day_time_patterns.loc[:,'weekday'] = pd.Categorical(day_time_patterns['weekday'], order_of_weekdays)
-
+day_time_patterns.loc_label = pd.Categorical(day_time_patterns.loc_label)
 day_time_patterns.head()
 
 
-# In[261]:
+# # Show location by time of day (seaborn plot)
 
-day_time_patterns.hr = day_time_patterns.hr.astype(float)
-day_time_patterns.dtypes
-
-
-# #Show location by time of day (seaborn plot)
-
-# In[262]:
-
-sns.set(style="whitegrid", color_codes=True)
-sns.set_context({"figure.figsize": (10, 7)})
-
-axes = sns.stripplot(x="weekday", y="hr", hue="loc_label", data=day_time_patterns, jitter=True);
-axes.set_ylim(0,25)
-
-labels = ["Midnight", "5am", "10am", "3pm", "8pm", "Midnight"]
-axes.set_yticklabels(labels)
-
-axes.legend(["Home","Other","Work"],loc='center left', bbox_to_anchor=(1, 0.5), fancybox=True, shadow=True)
-
-
-# In[263]:
+# In[394]:
 
 def get_day_num(day): 
     if day == 'Monday':
@@ -309,16 +306,35 @@ def get_day_num(day):
 day_time_patterns['day_value'] = day_time_patterns.weekday.astype(str).map(lambda x: get_day_num(x))
 
 
-# In[264]:
+# In[608]:
 
 day_time_patterns.rename(columns={'loc_label': 'Location'}, inplace=True)
-day_time_patterns.head(10)
+day_time_patterns.head()
 
 
-# In[265]:
+# # Day of week by time of day for location
+
+# In[716]:
 
 weekday_label = ("Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun")
 time_label = ("Midnight", "5am", "10am", "3pm", "8pm", "Midnight")
 
 ggplot(day_time_patterns, aes('day_value', 'hr', color='Location')) +     geom_point() +     scale_x_continuous(name = "Day of Week", breaks=range(1,8), labels=weekday_label) +     scale_y_continuous(name="Time of Day", limits=(0,25), labels=time_label)
+
+
+# In[764]:
+
+meat = meat.dropna(thresh=800, axis=1) # drop columns that have fewer than 800 observations
+ts = meat.set_index(['date'])
+ts.head(10)
+
+
+# In[765]:
+
+ts.groupby(ts.index.year).sum().head(10)
+
+
+# In[ ]:
+
+
 
